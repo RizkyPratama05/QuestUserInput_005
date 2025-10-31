e2406bd87daaef0444a4d2fa22334ab739b2c012
RizkyPratama05 <rizkypp428@gmail.com>
2025-10-31 15:51:50 +0700

    membuat horizontal divider

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -94,3 +94,11 @@ fun FormDataDiri(modifier: Modifier){
                    textAlamat = it
                }
            )
+           HorizontalDivider(
+               modifier = Modifier.padding(
+                   bottom = dimensionResource(R.dimen.padding_medium),
+                   top = dimensionResource(id = R.dimen.padding_medium)
+               ),
+               thickness = dimensionResource(id = R.dimen.divider_tipis),
+               color = Color.Black
+           )
