8263611990fb4fd8a22cdc74c5eac11f0b209199
RizkyPratama05 <rizkypp428@gmail.com>
2025-10-31 14:33:15 +0700

    first commit

---
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -0,0 +1,21 @@
+package com.example.pertemuan5
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier){
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textJk by remember { mutableStateOf(value = "") }
+
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jk by remember { mutableStateOf(value = "") }
+
+    val gender: List<>
+}
