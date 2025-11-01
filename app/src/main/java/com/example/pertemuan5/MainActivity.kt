edde6f9e0e82e1f69b9baf5a24c9b729a78eb62e
RizkyPratama05 <rizkypp428@gmail.com>
2025-11-01 14:52:23 +0700

    membuat file baru dan membuat fun form registrassi

--- app/src/main/java/com/example/pertemuan5/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan5/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan5Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    FormDataDiri(
+                    FormRegistrasi(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
