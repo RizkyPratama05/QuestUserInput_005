42055bfff119401fb7f841c2f7a21e7dc7b348a6
RizkyPratama05 <rizkypp428@gmail.com>
2025-11-01 14:55:41 +0700

    membuat var dan val danjuga popup validasi berhasil

--- app/src/main/java/com/example/pertemuan5/Prak5.kt
+++ app/src/main/java/com/example/pertemuan5/Prak5.kt
@@ -52,6 +52,61 @@ import java.util.*
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormRegistrasi(modifier: Modifier) {
+    Box {
+        var textNama by remember { mutableStateOf(value = "") }
+        var textAsal by remember { mutableStateOf(value = "") }
+        var textRT by remember { mutableStateOf(value = "") }
+        var textRW by remember { mutableStateOf(value = "") }
+        var textUmur by remember { mutableStateOf(value = "") }
+        var textTanggal by remember { mutableStateOf(value = "") }
+        var textJk by remember { mutableStateOf(value = "") }
+
+        var nama by remember { mutableStateOf(value = "") }
+        var asal by remember { mutableStateOf(value = "") }
+        var rt by remember { mutableStateOf(value = "") }
+        var rw by remember { mutableStateOf(value = "") }
+        var umur by remember { mutableStateOf(value = "") }
+        var tanggal by remember { mutableStateOf(value = "") }
+        var jk by remember { mutableStateOf(value = "") }
+
+        var showDialog by remember { mutableStateOf(false) }
+        if (showDialog) {
+            androidx.compose.material3.AlertDialog(
+                onDismissRequest = { showDialog = false },
+                title = {
+                    Text(
+                        text = "Berhasil",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 22.sp
+                    )
+                },
+                text = {
+                    Column {
+                        Text("Nama: $textNama")
+                        Text("Asal Kota: $textAsal")
+                        Text("Tanggal Lahir: ${textTanggal}")
+                        Text("RT: $textRT")
+                        Text("RW: $textRW")
+                        Text("Umur: $textUmur")
+                        Text("Jenis Kelamin: $textJk")
+                    }
+                },
+                confirmButton = {
+                    Button(onClick = { showDialog = false }) {
+                        Text("OK")
+                    }
+                }
+            )
+        }
+
+        val gender: List<String> = listOf("Laki-laki", "Perempuan")
+        val validasi = remember { mutableStateOf(false) }
+        var showDatePicker by remember { mutableStateOf(false) }
+        val datePickerState = rememberDatePickerState()
+        val selectedDate = datePickerState.selectedDateMillis?.let {
+            convertMillisToDate(it)
+        } ?: ""
+
 
 
 
